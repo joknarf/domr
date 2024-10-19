300ce345a94fa3ded3d855ef6a34eae1281c1838
joknarf <joknarf@free.fr>
2024-10-19 11:41:57 +0200

    plugin.bash w/o python

--- domr.plugin.bash
+++ domr.plugin.bash
@@ -1,1 +1,2 @@
-alias domr="${BASH_SOURCE%.plugin.bash}.py"
+type python >/dev/null 2>&1 && alias domr="${BASH_SOURCE%.plugin.bash}.py" && return
+type python3 >/dev/null 2>&1 && alias domr="python3 ${BASH_SOURCE%.plugin.bash}.py"
